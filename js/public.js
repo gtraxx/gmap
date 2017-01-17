86bd1678c4f507ab2fd38b7a3b145530d56f6b16
gtraxx <gerits.aurelien@gmail.com>
2017-01-17 10:31:06 +0100

    Fix bug meta default
    * Add support ssl
    * gmap3 v7 work

--- js/public.js
+++ js/public.js
@@ -28,6 +28,151 @@ var gmap = (function ($, undefined) {
             position: center,
             content: infocontent
         });
+        if(item.route == '1'){
+            /*$('#getadress').autocomplete({
+                //This bit uses the geocoder to fetch address values
+                source: function(request, response) {
+                    $("#map_adress").gmap3({
+                        address: request.term
+                    }).then(function(results){
+                        if (!results) return;
+                        response($.map(results, function(item) {
+                            return {
+                                label: item.formatted_address,
+                                value: item.formatted_address,
+                                latLng: item.geometry.location
+                            };
+                        }));
+                    });
+                }
+            });*/
+            $('#getadress').autocomplete({
+                //This bit uses the geocoder to fetch address values
+                source: function(request, response) {
+                    /*$("#map_adress").gmap3({
+                        address: request.term
+                    }).then(function(results){
+                        if (!results) return;
+                        response($.map(results, function(item) {
+                            return {
+                                label: item.formatted_address,
+                                value: item.formatted_address,
+                                latLng: item.geometry.location
+                            };
+                        }));
+                    });*/
+                    var geocoder = new google.maps.Geocoder();
+                    geocoder.geocode({'address': request.term}, function (results, status) {
+                        if (status === google.maps.GeocoderStatus.OK) {
+                            console.log(results);
+                        }
+                    });
+                }
+            });
+
+        }
     }
 
+    /**
+     * Trace la route sur la carte
+     * @param button
+     * @param item
+     */
+    function getDirection(button,item){
+        if(item.route == '1'){
+            $(document).on('click',button,function(event){
+                $('#r-directions').empty();
+                if($('#getadress').val().length != 0){
+                    $('#r-directions').addClass('sizedirection').show(800);
+                    /*$('#map_adress').gmap3({
+                        clear: {name:"getroute"},
+                        getroute:{
+                            options:{
+                                origin: $('#getadress').val(),
+                                destination: item.adress+' '+item.city+','+item.country,
+                                travelMode: google.maps.DirectionsTravelMode.DRIVING
+                            },
+                            callback: function(results){
+                                if (!results) return;
+                                $(this).gmap3({
+                                    directionsrenderer:{
+                                        container: $('#r-directions'),
+                                        options:{
+                                            directions:results
+                                        }
+                                    }
+                                });
+                            }
+                        }
+                    });*/
+                    $('#map_adress')
+                        .gmap3({
+                            center: [item.lat, item.lng],
+                            zoom: 6,
+                            mapTypeId : google.maps.MapTypeId.ROADMAP
+                        })
+                        .route({
+                            origin: $('#getadress').val(),
+                            destination: item.adress+' '+item.city+','+item.country,
+                            travelMode: google.maps.DirectionsTravelMode.DRIVING
+                        })
+                        .directionsrenderer(function (results) {
+                            if (results) {
+                                return {
+                                    panel: "#r-directions",
+                                    directions: results
+                                }
+                            }
+                        });
+                    $('#getadress').val('');
+                }
+            });
+        }
+    }
+
+    /**
+     * Chargement des données de la carte au formùat JSON
+     * @param button
+     * @param iso
+     */
+    function jsonData(button,iso){
+        $.nicenotify({
+            ntype: "ajax",
+            uri: '/plugins.php?strLangue='+iso+'&magixmod=gmap&jsondata=true',
+            typesend: 'get',
+            datatype: 'json',
+            beforeParams:function(){
+                var img = $(document.createElement("img"))
+                    .addClass("map_loader")
+                    .attr("src", "/plugins/gmap/img/ajax-loader.gif");
+                $('#map_adress').html(img);
+            },
+            successParams:function(j){
+                $('#map_adress').empty();
+                $.nicenotify.initbox(j,{
+                    display:false
+                });
+                if(j === undefined){
+                    console.log(j);
+                    $('#map_adress').text("map is undefined");
+                }
+                if(j !== null){
+                    $.each(j, function(i,item) {
+                        if(item.lat != null && item.lng != null){
+                            loadMap(item);
+                            if(item.adress != null && item.city != null){
+                                getDirection(button,item);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+    return {
+        //Fonction Public
+        run:function(iso){
+            jsonData('.subdirection:button',iso);
+        }
+    };
 })(jQuery);
--- skin/public/index.tpl
+++ skin/public/index.tpl
@@ -1,18 +1,24 @@
 {extends file="layout.tpl"}
 {block name="styleSheet" append}
-    <script src="http://maps.google.com/maps/api/js?sensor=false&amp;language={getlang}{if $config_map.api_key != '' OR $config_map.api_key != NULL}&amp;key={$config_map.api_key}{/if}" type="text/javascript"></script>
+    <script src="{if isset($smarty.server.HTTPS) eq 'on'}https{else}http{/if}://maps.google.com/maps/api/js?sensor=false&amp;language={getlang}{if $config_map.api_key != '' OR $config_map.api_key != NULL}&amp;key={$config_map.api_key}{/if}" type="text/javascript"></script>
     {headlink rel="stylesheet" href="/min/?f=plugins/gmap/css/perfect-scrollbar.min.css,plugins/gmap/css/ui-bootstrap/jquery-ui-1.10.3.custom.css" concat=$concat media="screen"}
     <!--[if lt IE 9]>
     {headlink rel="stylesheet" href="/min/?f=plugins/gmap/css/ui-bootstrap/jquery.ui.1.10.3.ie.css" concat=$concat media="screen"}
     <![endif]-->
 {/block}
-{block name="title"}{seo_rewrite config_param=['level'=>'0','idmetas'=>'1','default'=>{$name_map|ucfirst}]}{/block}
-{block name="description"}{seo_rewrite config_param=['level'=>'0','idmetas'=>'2','default'=>{$name_map|ucfirst}]}{/block}
+{block name="title"}{seo_rewrite config_param=['level'=>'0','idmetas'=>'1','default'=>{$config_map.name_map|ucfirst}]}{/block}
+{block name="description"}{seo_rewrite config_param=['level'=>'0','idmetas'=>'2','default'=>{$config_map.name_map|ucfirst}]}{/block}
 {block name='body:id'}gmap{/block}
 {block name="main"}
     {$map_data}
 {/block}
 {block name="foot" append}
+    {script src="/min/?f=libjs/jquery-ui.1.10.4.min.js" concat=$concat type="javascript"}
+    {script src="/min/?g=form" concat=$concat type="javascript"}
+    {capture name="formjs"}{strip}
+        /min/?f=skin/{template}/js/form.min.js
+    {/strip}{/capture}
+    {script src=$smarty.capture.formjs concat=$concat type="javascript" load='async'}
     {script src="/min/?f=plugins/gmap/js/perfect-scrollbar.min.js,plugins/gmap/js/gmap3.min.js,plugins/gmap/js/public.0.3.js" concat=$concat type="javascript"}
     {if $plugin_status != 0}
         <script type="text/javascript">
